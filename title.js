1cdf1279b0b72106f87cd832c586aa54daa0e8c6
MxweIlCatDEV <141961472+SandStormDEVOfficial@users.noreply.github.com>
2023-11-29 17:23:34 +0300

    Update title.js

--- title.js
+++ title.js
@@ -1,2 +1,26 @@
-var title = document.title;
-document.title = 'Новый заголовок страницы';
+<button id="start">Запустить</button>
+<button id="stop">Остановить</button>
+ 
+<script>
+var timer;
+var start_title = document.title;
+ 
+$('#start').click(function(){
+	for (i = 0; i < 5; i++) {
+		document.title = document.title + ' | ' + start_title;
+	}	
+	
+	timer = setInterval(function(){
+		lenta = document.title;
+		if (lenta.length == 100){
+			lenta = lenta + ' | ' + start_title;
+		}
+		document.title = lenta.substr(1, lenta.length);
+	}, 100);
+});
+ 
+$('#stop').click(function(){
+	clearInterval(timer);
+	document.title = start_title;
+});
+</script>
